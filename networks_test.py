01ab8bbd4c4ae37742df6f5e20aa9a5eea956289
方晓燕 <13687484390@163.com>
2017-04-28 15:58:13 +0800

    Update networks_test.py

--- networks_test.py
+++ networks_test.py
@@ -7,6 +7,7 @@ Created on Thu Apr 27 20:57:52 2017
 import numpy as np
 def net_works_test(x,W,b):
     l = len(W)
+    f = lambda x:1/(1+np.exp(-x)) # 这里使用sigmoid函数作为每一层的激活函数
     m,n = x.shape
     for i in range(0,l):
         x = f(np.dot(x,W[i]) + np.dot(np.ones((m,1)),b[i].T))
